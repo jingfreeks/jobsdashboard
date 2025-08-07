191ae932eeba28ff8aac673d582f84c71a0954e4
lyndell Dobluis <lyndell.dobluis@gmail.com>
2025-08-07 08:51:33 +0800

    integrate on boarding features

--- src/config/apiSplice.tsx
+++ src/config/apiSplice.tsx
@@ -62,10 +62,11 @@ const baseQueryWithAuth = async (
   return result;
 };
 
-export const apiSlice = createApi<any, any>({
+export const apiSlice = createApi({
   baseQuery: baseQueryWithAuth,
   reducerPath: 'api',
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   endpoints: _ => ({}),
-          tagTypes: ['City', 'Jobs', 'MyJobs', 'JobDetails', 'Profile', 'Bank', 'State', 'Shift'] as any,
+  tagTypes: ['City', 'Jobs', 'MyJobs', 'JobDetails', 'Profile', 'Bank', 'State', 'Shift', 'Onboarding'] as const,
+  keepUnusedDataFor: 60,
 });
